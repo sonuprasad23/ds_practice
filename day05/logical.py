0ad8ee1f51e2d257cf1383fb52d7261db38a00df
Sonu Prasad <sonubab2222@gmail.com>
2025-04-24 15:09:08 +0530

    Added logical.py

---
+++ day05/logical.py
@@ -0,0 +1,8 @@
+# day05/logical.py
+is_valid = True
+is_admin = False
+
+print(f"is_valid and is_admin: {is_valid and is_admin}")
+print(f"is_valid or is_admin: {is_valid or is_admin}")
+print(f"not is_admin: {not is_admin}")
+print(f"not is_valid: {not is_valid}")
