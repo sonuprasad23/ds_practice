72ba5fb3dae9ed0e8ab8bf392973d3c1659dba16
Sonu Prasad <sonubab2222@gmail.com>
2025-04-24 15:00:34 +0530

    Added Arithmetic Python

---
+++ day05/arithmetic.py
@@ -0,0 +1,11 @@
+# day05/arithmetic.py
+a = 15.0
+b = 4
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")   # True division
+print(f"{a} // {b} = {a // b}") # Floor division
+print(f"{a} % {b} = {a % b}")   # Modulo
+print(f"{a} ** {b} = {a ** b}") # Exponentiation
