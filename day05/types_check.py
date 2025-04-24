3abfa4365948fb3c75d665b7415cc22964925457
Sonu Prasad <sonubab2222@gmail.com>
2025-04-24 14:59:40 +0530

    Types check.py

---
+++ day05/types_check.py
@@ -0,0 +1,18 @@
+# day05/types_check.py
+my_int = 10
+my_float = 3.14
+my_str = "Learning Python"
+my_bool = True
+my_list = [1, "a", 3.0]
+my_tuple = (1, "a", 3.0)
+my_dict = {"course": "Python", "day": 5}
+my_set = {1, 2, 3, 3, 2} # Duplicates ignored
+
+print(f"my_int: {my_int}, Type: {type(my_int)}")
+print(f"my_float: {my_float}, Type: {type(my_float)}")
+print(f"my_str: {my_str}, Type: {type(my_str)}")
+print(f"my_bool: {my_bool}, Type: {type(my_bool)}")
+print(f"my_list: {my_list}, Type: {type(my_list)}")
+print(f"my_tuple: {my_tuple}, Type: {type(my_tuple)}")
+print(f"my_dict: {my_dict}, Type: {type(my_dict)}")
+print(f"my_set: {my_set}, Type: {type(my_set)}")
