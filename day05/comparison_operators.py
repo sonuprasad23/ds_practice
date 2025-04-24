f62f516ed69b4c4110d1f7d853e394d966b66482
Sonu Prasad <sonubab2222@gmail.com>
2025-04-24 15:08:16 +0530

    Comparison Operators

---
+++ day05/comparison_operators.py
@@ -0,0 +1,10 @@
+# day05/comparisons.py
+x = 10
+y = 20
+
+print(f"{x} == {y}: {x == y}")
+print(f"{x} != {y}: {x != y}")
+print(f"{x} > {y}: {x > y}")
+print(f"{x} < {y}: {x < y}")
+print(f"{x} >= 10: {x >= 10}")
+print(f"{y} <= 10: {y <= 10}")
